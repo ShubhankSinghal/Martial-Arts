f9f2a9a22966644508b6b19f3dad8658b27696aa
Sakshi Sharma <64321272+ssharma2303@users.noreply.github.com>
2021-05-02 18:24:03 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ let loadImage = (src, callback) => {
 }
 
 var bkGround = new Image();
-bgImage.src = 'https://github.com/ssharma2303/Martial-Arts/blob/main/images/background.jpg';
+bkGround.src = 'https://github.com/ssharma2303/Martial-Arts/blob/main/images/background.jpg';
 bkGround.onload = () => {
 ctx.drawImage(bkGround,400,1261); 
 }
