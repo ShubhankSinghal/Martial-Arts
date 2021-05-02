c86b3a98923a09683e4a2d5e58dc1a87e923b0ec
Sakshi Sharma <64321272+ssharma2303@users.noreply.github.com>
2021-05-02 10:41:45 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -47,12 +47,10 @@ let loadImages = (callback) => {
 
 let animate = (ctx, images, animation, callback) => {
     images[animation].forEach((image, index) => {
-        if(images[animation] === "kick") {
         setTimeout(()=>{
             ctx.clearRect(0, 0, 250, 300);
-            ctx.drawImage(image, 260, 310, 250, 300);
+            ctx.drawImage(image, 0, 0, 250, 300);
         }, index * 100);
-        }
     });
     setTimeout(callback, images[animation].length * 100);
 };
